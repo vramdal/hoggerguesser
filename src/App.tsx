8efdfa48cc23f3658bd1bb5e781ff4fdd82574be
Vidar Ramdal <vramdal@gmail.com>
2022-09-16 00:52:28 +0200

    Fungerende kart og sangliste

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { ReactNode, RefObject, useEffect } from 'react';
 import './App.scss';
 import 'leaflet/dist/leaflet.css';
 import { Feature, MapMarkerSpec, Song } from "./types";
-import { MapContainer, Marker, Popup, TileLayer, } from 'react-leaflet';
-import { Icon, LatLngBounds } from "leaflet";
+import { MapContainer, Marker, Popup, TileLayer, Tooltip, useMap, } from 'react-leaflet';
+import {
+  Icon,
+  LatLngBounds,
+  Map as LeafletMap,
+  Popup as LeafletPopup,
+  Tooltip as LeafletTooltip
+} from "leaflet";
 import classNames from "classnames";
 
 type SongPlace = {
@@ -24,7 +30,7 @@ type PlaceMapRecord = { title: string, highlighted: boolean, songs: Array<{ id: 
 
 type PlaceMap = { [key: number]: PlaceMapRecord };
 
-type MapMarkerDisplay = MapMarkerSpec & { highlighted: boolean };
+type MapMarkerDisplay = MapMarkerSpec & { highlighted: boolean, popup: RefObject<LeafletPopup>, tooltip: RefObject<LeafletTooltip> };
 
 function App() {
 
@@ -43,11 +49,12 @@ function App() {
   const [bySongId, setBySongId] = React.useState<MapBySongId>({});
   const [byPlaceId, setByPlaceId] = React.useState<PlaceMap>({});
   const [songDisplay, setSongDisplay] = React.useState<Array<Song & {highlighted: boolean}>>([]);
-  const [bounds, setBounds] = React.useState<LatLngBounds>()
+  const [bounds, setBounds] = React.useState<LatLngBounds>();
+  const [mapRef, setMapRef] = React.useState<LeafletMap | null>(null);
 
   const featureToMarker: (feature: Feature) => MapMarkerDisplay = (feature: Feature) => {
     const coordinates = feature.geometry.coordinates;
-    const {radius, title, id} = feature.properties;
+    const { title, id} = feature.properties;
     return ({
       position: {
         lat: coordinates[1],
@@ -56,6 +63,8 @@ function App() {
       id,
       InfoWindowContent: `${title}`,
       highlighted: false,
+      popup: React.createRef(),
+      tooltip: React.createRef(),
     });
   }
 
@@ -117,10 +126,18 @@ function App() {
   }, [songs]);
 
   function highlightSongs(songs: Array<{ id: number; title: string }>) {
-    const newSongDisplay = songDisplay.map(song => ({
-      ...song,
-      highlighted: songs.find(s => s.id === song.id) !== undefined
-    }));
+
+    const newSongDisplay = songDisplay.map(song => {
+      const shouldBeHighlighted = songs.find(s => s.id === song.id) !== undefined;
+      if (song.highlighted !== shouldBeHighlighted) {
+        return ({
+          ...song,
+          highlighted: shouldBeHighlighted
+        });
+      } else {
+        return song;
+      }
+    });
     setSongDisplay(newSongDisplay);
   }
 
@@ -152,19 +169,34 @@ function App() {
     }
   }
 
-  const adjustToViewBounds = (coordinates: Array<[number, number]>) => {
-    const bounds = new LatLngBounds(coordinates[0], coordinates[0]);
-    // map.fitBounds(bounds, {padding: [20, 20]});
+  const adjustToViewBounds = (coordinatesList: Array<[number, number]>) => {
+    const [firstPlace, ...otherPlaces] = coordinatesList;
+    const place1 = {lng: firstPlace[0], lat: firstPlace[1]};
+    const place2 = {lng: firstPlace[0] , lat: firstPlace[1]};
+    const bounds = new LatLngBounds(place1, place2);
+    for (const place of otherPlaces) {
+      bounds.extend({lng: place[0], lat: place[1]});
+    }
     setBounds(bounds);
   }
 
+  function highlightPlaces(placesIds: Array<number>) {
+    const allTooltips = mapMarkers.map(marker => marker.tooltip.current!.getElement()!);
+    allTooltips.forEach(tooltip => tooltip.classList.remove("highlighted"));
+    const markers = mapMarkers.filter(marker => placesIds.includes(marker.id));
+    markers.forEach(marker => marker.tooltip.current!.getElement()!.classList.add("highlighted"));
+  }
+
   const songClicked = (songId: number) => {
-    console.log('songId = ', songId);
     const newSongDisplay = songDisplay.map(song => ({...song, highlighted: song.id === songId}));
     setSongDisplay(newSongDisplay);
     const places = bySongId[songId].places;
-    highlightMarkers(places.map(place => place.id));
+    const placeIds = places.map(place => place.id);
+    highlightMarkers(placeIds);
+    highlightPlaces(placeIds);
     adjustToViewBounds(places.map(place => place.coordinates));
+    scrollSongIntoView(songId);
+    mapRef!.closePopup();
   }
 
   return (
@@ -176,9 +208,9 @@ function App() {
           <tbody>
           {songDisplay.map(song =>
             <tr key={song.id}
-                className={classNames("song-row", {highlighted: song.highlighted})}
+                className={classNames("song-row", {highlighted: song.highlighted, disabled: song.places.length === 0, enabled: song.places.length > 0})}
                 id={"song-" + song.id}>
-              <td><a onClick={() => songClicked(song.id)}>{song.title}</a></td>
+              <td onClick={() => songClicked(song.id)}>{song.title}</td>
             </tr>)}
           </tbody>
         </table>
@@ -189,33 +221,60 @@ function App() {
           10.698810319462064
         ]
         } zoom={10} scrollWheelZoom={true} bounds={bounds}
+                      ref={setMapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {mapMarkers.map(marker => {
-            const icon : Icon = new Icon({
-              iconUrl: "/zlatkodesign-Car-wheel.png",
-              iconSize: (marker.highlighted ? [30,30] : [20, 20]),
-              attribution: "Zlatko Design - https://freesvg.org/car-wheel-in-gray-color",
-              className: classNames("map-marker", {highlighted: marker.highlighted})
-            });
-            return <Marker eventHandlers={{click: () => {
-              markerClicked(marker.id);
-            }}} icon={icon} position={[marker.position.lat, marker.position.lng]} key={marker.id} title={marker.InfoWindowContent}>
-              <Popup>
-                <h3>{marker.InfoWindowContent}</h3>
-                <ul>
-                  {byPlaceId[marker.id].songs.map(song => <li key={song.id} onClick={() => songClicked(song.id)}>{song.title}</li>)}
-                </ul>
-              </Popup>
-            </Marker>;
-          })}
+          <MyMapContainer bounds={bounds}>
+            <>
+              {mapMarkers.map(marker => {
+                const icon: Icon = new Icon({
+                  iconUrl: "/zlatkodesign-Car-wheel.png",
+                  iconSize: (marker.highlighted ? [30, 30] : [20, 20]),
+                  attribution: "Zlatko Design - https://freesvg.org/car-wheel-in-gray-color",
+                  className: classNames("map-marker", {highlighted: marker.highlighted}),
+                  tooltipAnchor: [0, 0]
+                });
+                return <Marker eventHandlers={{
+                  click: () => {
+                    markerClicked(marker.id);
+                  }
+                }} icon={icon} position={[marker.position.lat, marker.position.lng]} key={marker.id}
+                               interactive={true}
+                               title={marker.InfoWindowContent}>
+                  <Popup ref={marker.popup} className={"map-marker-popup"} >
+                    <h3>{marker.InfoWindowContent}</h3>
+                    <ul>
+                      {byPlaceId[marker.id].songs.map(song => <li key={song.id}
+                                                                  onClick={() => songClicked(song.id)}>{song.title}</li>)}
+                    </ul>
+                  </Popup>
+                  <Tooltip className={classNames("map-marker-tooltip", {highlightedLaks: marker.highlighted})} direction={"top"} permanent ref={marker.tooltip}>{marker.InfoWindowContent}</Tooltip>
+                </Marker>;
+              })}
+            </>
+          </MyMapContainer>
         </MapContainer>
       </main>
     </div>
   );
 }
+
+const MyMapContainer = ({bounds, children } : {bounds?: LatLngBounds, children: ReactNode}) => {
+
+  const myMap = useMap();
+
+  useEffect(() => {
+    if (bounds) {
+      myMap.fitBounds(bounds, {padding: [20, 20], animate: true, maxZoom: 15});
+    }
+  }, [bounds, myMap]);
+
+
+  return <>{children}</>;
+};
+
 
 export default App;
