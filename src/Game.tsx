34ee096064980db57aa6926706fd000b4b72b699
Vidar Ramdal <vramdal@gmail.com>
2022-09-18 15:17:06 +0200

    Unngå å få samme sang flere ganger

--- src/Game.tsx
+++ src/Game.tsx
@@ -6,9 +6,19 @@ import classNames from "classnames";
 
 type AnswerStatus = 'CORRECT' | 'WRONG' | undefined;
 
+const SHOW_WELCOME_SCREEN = -1;
+const ROUND_COUNT = 5;
+
 type GameContextType = { advanceGame: () => void, correctSong: Song, showTooltipHints: () => void };
 const GameContext = React.createContext<GameContextType | undefined>(undefined);
 
+type Round = {
+  correctSong: Song,
+  answeredSong?: Song,
+  status: AnswerStatus
+  points: number,
+  hintsUsed: number
+};
 const Game = ({
                 songs,
                 zoomToSong,
@@ -22,33 +32,36 @@ const Game = ({
               }
 ) => {
 
-
-  const [gameNum, setGameNum] = useState<number>(0);
+  const [roundNum, setRoundNum] = useState<number>(SHOW_WELCOME_SCREEN);
   const [correctSong, setCorrectSong] = useState<Song | undefined>(undefined);
   const [candidates, setCandidates] = useState<Array<Song>>([]);
-  const [rounds, setRounds] = useState<Array<{
-    correctSong: Song,
-    answeredSong?: Song,
-    status: AnswerStatus
-    points: number,
-    hintsUsed: number}>>([]);
+  const [rounds, setRounds] = useState<Array<Round>>([]);
   const [totalPoints, setTotalPoints] = useState<number>(0);
+  const [shuffledSongList, setShuffledSongList] = useState<Array<Song>>([]);
+  const [gameNum, setGameNum] = useState<number>(-1);
 
   useEffect(() => {
     const songsWithPlaces = songs.filter(song => song.places.length > 0);
     const shuffledSongList = songsWithPlaces.sort(() => Math.random() - 0.5);
-    const correctSong = shuffledSongList[0];
-    const selectedSongPlaces = correctSong.places;
-    const excludedSongs = shuffledSongList.filter(song => song.places.every(place => selectedSongPlaces.includes(place)));
-    const candidateSongs = shuffledSongList.filter(song => !excludedSongs.includes(song)).slice(0, 4).concat(correctSong);
-    setCandidates(candidateSongs.sort(() => Math.random() - 0.5));
-    setCorrectSong(correctSong);
-    const newRound = {correctSong, points: 0, hintsUsed: 0, status: undefined};
-    setRounds(rounds => {
-      rounds[gameNum] = newRound;
-      return [...rounds];
-    });
-  }, [gameNum, songs]);
+    setShuffledSongList(shuffledSongList);
+  }, [songs, gameNum]);
+
+
+  useEffect(() => {
+    if (roundNum > -1 && roundNum < ROUND_COUNT) {
+      const correctSong = shuffledSongList[roundNum];
+      const selectedSongPlaces = correctSong.places;
+      const excludedSongs = shuffledSongList.filter(song => song.places.every(place => selectedSongPlaces.includes(place)));
+      const candidateSongs = shuffledSongList.filter(song => !excludedSongs.includes(song)).slice(0, 4).concat(correctSong);
+      setCandidates(candidateSongs.sort(() => Math.random() - 0.5));
+      setCorrectSong(correctSong);
+      const newRound = {correctSong, points: 0, hintsUsed: 0, status: undefined};
+      setRounds(rounds => {
+        rounds[roundNum] = newRound;
+        return [...rounds];
+      });
+    }
+  }, [roundNum, shuffledSongList]);
 
   useEffect(() => {
     setTotalPoints(rounds.reduce((acc, round) => acc + round.points, 0));
@@ -62,12 +75,12 @@ const Game = ({
   }, [correctSong, zoomToSong]);
 
   const advanceGame = React.useCallback(() => {
-    setGameNum(gameNum => gameNum + 1);
+    setRoundNum(roundNum => roundNum + 1);
     removeHighlighting();
   }, [removeHighlighting]);
 
-  const handleAnswer = React.useCallback((gameNum: number, answeredSongId: number, points: number) => {
-    const round = rounds[gameNum];
+  const handleAnswer = React.useCallback((roundNum: number, answeredSongId: number, points: number) => {
+    const round = rounds[roundNum];
     const answeredSong = songs.find(song => song.id === answeredSongId);
     console.log("answeredSong = ", answeredSong!.title, "correctSongId = ", round.correctSong.title);
     let status : AnswerStatus;
@@ -77,22 +90,34 @@ const Game = ({
       status = 'WRONG';
       points = 0;
     }
-    setRounds([...rounds.slice(0, gameNum), {...round, answeredSong, points, status}, ...rounds.slice(gameNum + 1)]);
+    setRounds([...rounds.slice(0, roundNum), {...round, answeredSong, points, status}, ...rounds.slice(roundNum + 1)]);
     showSongTooltips(round.correctSong.id);
   }, [rounds, songs, showSongTooltips, setRounds]);
 
+  const startGame = React.useCallback(() => {
+    setRounds([]);
+    setRoundNum(0);
+    setGameNum(gameNum => gameNum + 1);
+  }, [setRoundNum]);
+
   return <>
     <GameContext.Provider value={{advanceGame, correctSong: correctSong!, showTooltipHints: () => showSongTooltips(correctSong!.id)}}>
-      <Control position="topright">
-        <h1>Poengsum: {totalPoints}</h1>
-      </Control>
+      {roundNum === SHOW_WELCOME_SCREEN && <GameWelcome startGame={startGame}/>}
+      {roundNum > SHOW_WELCOME_SCREEN && roundNum < ROUND_COUNT  &&
+          <>
+              <Control position="topright">
+                  <h1>Poengsum: {totalPoints}</h1>
+              </Control>
 
-      <GameRound
-        gameNum={gameNum}
-        candidates={candidates}
-        answerSelected={handleAnswer}
-        status={rounds[gameNum]?.status || undefined}
-      />
+              <GameRound
+                  roundNum={roundNum}
+                  candidates={candidates}
+                  answerSelected={handleAnswer}
+                  status={rounds[roundNum]?.status || undefined}
+              />
+          </>
+      }
+      {roundNum === ROUND_COUNT && <GameSummary totalPoints={totalPoints} rounds={rounds} startGame={startGame}/>}
     </GameContext.Provider>
   </>
 }
@@ -113,22 +138,47 @@ const GameRoundResult = (props: {answerStatus: AnswerStatus, answeredSong: Song}
         <h1>Riktig</h1>
     </>}
     {props.answerStatus === 'WRONG' && <>
-    <h1>Feil</h1>
-    Riktig svar var <h2>{game.correctSong.title}</h2>
+        <h1>Feil</h1>
+        Riktig svar var <h2>{game.correctSong.title}</h2>
     </>}
     <button onClick={game.advanceGame}>Neste</button>
   </Control>
 };
 
+const GameWelcome = (props: {startGame: () => void}) => {
+  return <Control position="topright" container={{className: "game-welcome"}}>
+    <h1>Hvor godt kjenner du Vazelina-geografien?</h1>
+    <p>I dette spillet blir du plassert foran et kart, hvor ett eller flere steder er merket.</p>
+    <p>Hvilken sang er det som nevner disse stedene? Velg riktig alternativ, og du får poeng.
+      Du får flere poeng jo raskere du svarer.</p>
+    <p>Du får {ROUND_COUNT} spørsmål. Lykke til!</p>
+    <button onClick={props.startGame}>Start</button>
+  </Control>
+}
+
+const GameSummary = (props: {startGame: () => void, totalPoints: number, rounds: Array<Round>}) => {
+  return <Control position="topright" container={{className: "game-summary"}}>
+    <h1>Spillet er over!</h1>
+    <ol>
+      {props.rounds.map((round, index) => <li key={index}>
+        {round.correctSong.title} {round.status === 'CORRECT' && <span className="answer-correct">✅</span> || <span className="answer-wrong">❌</span>} <span>{round.points} poeng</span>
+      </li>)}
+    </ol>
+    <p>Du fikk <h2>{props.totalPoints} poeng</h2></p>
+    <p>Trykk på knappen for å starte på nytt.</p>
+    <button onClick={props.startGame}>Prøv igjen</button>
+  </Control>
+}
+
 const GameRound = ({
-                     gameNum,
+                     roundNum,
                      candidates,
                      answerSelected,
                      status,
                    }: {
-  gameNum: number,
+  roundNum: number,
   candidates: Array<Song>,
-  answerSelected: (gameNum : number, songId: number, bonus: number) => void,
+  answerSelected: (roundNum : number, songId: number, bonus: number) => void,
   status: AnswerStatus,
 }) => {
   const [countdown, setCountdown] = useState<number>(1000);
@@ -138,28 +188,28 @@ const GameRound = ({
   useEffect(() => {
     setCountdown(1000);
     setHintUsed(false);
-  }, [gameNum]);
+  }, [roundNum]);
 
   useEffect(() => {
     console.log("setting interval");
     const interval = setInterval(() => {
-        setCountdown(countdown => {
-          if (countdown > 500 && status === undefined) {
-            return countdown - 25;
-          } else {
-            return countdown;
-          }
-        });
+      setCountdown(countdown => {
+        if (countdown > 500 && status === undefined) {
+          return countdown - 25;
+        } else {
+          return countdown;
+        }
+      });
     }, 1000);
     return () => {
       console.log("clearing interval");
       clearInterval(interval);
     };
-  }, [status, gameNum]);
+  }, [status, roundNum]);
 
   const answerButtonClicked = (song: Song) => {
     setAnsweredSong(song);
-    answerSelected(gameNum, song.id, countdown );
+    answerSelected(roundNum, song.id, countdown );
   };
 
   const game = useGame();
@@ -187,8 +237,6 @@ const GameRound = ({
         </button>
       </p>
     </Control></>
-
-
 }
 
 const useGame = (): GameContextType => {
